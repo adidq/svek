7f86e9d7a19db190ef388476322c3472eaa4229a
jky785 <145259647+jky785@users.noreply.github.com>
2024-03-13 22:58:48 +0000

    bootstrap

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,31 +1,26 @@
-<nav>
-  <div class="signedInStatus">
-    <div class="nojs-show loaded">
-      <img alt="User avatar" src={$page.data?.session?.user?.image ?? "https://source.boringavatars.com/marble/120"} class="avatar"/>
-      {#if $page.data.session}
-        <span class="signedInText">
-          {$page.data.session.user?.email ?? $page.data.session.user?.name}
-        </span>
-        <SignOut>
-          <div slot="submitButton" class="buttonPrimary">Sign out</div>
-        </SignOut>
-      {:else}
-        <SignIn>
-          <div slot="submitButton" class="buttonPrimary">Sign in</div>
-        </SignIn>
-      {/if}
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">about</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
 <script>
-  import { page } from "$app/stores"
-  import { SignIn, SignOut } from "@auth/sveltekit/components"
 </script>
-<style scoped>
-  nav {
-    display: flex;
-  }
-  img {
-    size: auto;
-  }
-</style>
