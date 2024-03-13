41895ea09f380ccaca3c23d65de75f74970ab097
jky785 <145259647+jky785@users.noreply.github.com>
2024-03-13 02:15:24 +0000

    route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<div>
+  <header>
+    <nav>
+      <a href="/">home</a>
+      <a href="/auth/signin">signin</a>
+    </nav>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <h1>footer</h1>
+  </footer>
+</div>
+<script>
+  
+</script>
