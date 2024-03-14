3c5c11a67c909be130cc80c795dde55e766d16be
jky785 <145259647+jky785@users.noreply.github.com>
2024-03-14 02:12:39 +0000

    Place holder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,27 @@
     import { page } from "$app/stores"
 </script>
 {#if $page.data.session}
-<h1>Hello <strong>{$page.data.session.user?.name}</strong>?</h1>
+<h1 class="gra1">Hello <strong>{$page.data.session.user?.name}</strong>?</h1>
+  <p class="placeholder-wave">
+    <span class="placeholder col-12"></span>
+  </p>
+  <p class="placeholder-wave">
+    <span class="placeholder col-12"></span>
+  </p>
 {:else}
 <h1>please login</h1>
 <p>
     <a href="/auth/signin"> You must be signed in to view this page </a>
 </p>
 {/if}
+<style scoped>
+  .gra1 {
+    font-size: 50px;
+    font-weight: bolder;
+
+    background-image: linear-gradient(to right, blue, skyblue);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+  }
+</style>
