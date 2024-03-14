402b1b38561e7266f42776d76f2018edd6da1f9f
jky785 <145259647+jky785@users.noreply.github.com>
2024-03-15 07:24:25 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import { page } from "$app/stores"
 </script>
 {#if $page.data.session}
-<div class="p-3 mb-2 bg-transparent text-body">.bg-transparent</div>
 <h1><strong class="gra1">Hello</strong> <strong>{$page.data.session.user?.name}</strong></h1>
 {/if}
 <div>
