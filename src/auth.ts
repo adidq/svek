3876aabfc56e757b42e21214e6a0ddd975001d00
jky785 <145259647+jky785@users.noreply.github.com>
2024-03-18 07:50:57 +0000

    d

--- src/auth.ts
+++ src/auth.ts
@@ -4,13 +4,15 @@ import Discord from "@auth/sveltekit/providers/discord"
 import type { Handle } from "@sveltejs/kit";
 import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_DISCORD_ID, AUTH_DISCORD_SECRET, AUTH_SECRET } from "$env/static/private"
 
+const scopes = ['identify', 'email']
+
 export const { handle, signIn, signOut } = SvelteKitAuth({
   theme: {
     logo: "https://sveltekit-supabase-dashboard.vercel.app/svelte.png",
   },
   providers: [
     Google({ clientId: AUTH_GOOGLE_ID, clientSecret: AUTH_GOOGLE_SECRET }),
-    Discord({ clientId: AUTH_DISCORD_ID, clientSecret: AUTH_DISCORD_SECRET })
+    Discord({ clientId: AUTH_DISCORD_ID, clientSecret: AUTH_DISCORD_SECRET, authorization: {params: {scope: scopes.join(' ')}} })
   ],
   secret: AUTH_SECRET,
   trustHost: true
