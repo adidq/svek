4b9b5a952b00dab22045b936ff8f907507419d1c
jky785 <145259647+jky785@users.noreply.github.com>
2024-03-18 06:48:35 +0000

    tt

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,7 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
 import Naver from "@auth/sveltekit/providers/naver"
-import type { Handle } from "@sveltejs/kit";
 import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_NAVER_ID, AUTH_NAVER_SECRET, AUTH_SECRET } from "$env/static/private"
-
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   theme: {
@@ -13,4 +11,6 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
     Google({ clientId: AUTH_GOOGLE_ID, clientSecret: AUTH_GOOGLE_SECRET }),
     Naver({ clientId: AUTH_NAVER_ID, clientSecret: AUTH_NAVER_SECRET })
   ],
+  secret: AUTH_SECRET,
+  trustHost: true
 })
--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/"><b>SVEK</b></a>
+    <a class="navbar-brand" href="/"><b>NAVBAR</b></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -11,7 +11,7 @@
         </li>
         {#if $page.data.session}
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               <strong>{$page.data.session.user?.email ?? $page.data.session.user?.name}</strong>
             </a>
             <ul class="dropdown-menu">
